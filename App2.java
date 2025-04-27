67a4ab1975cd49bd03910c9002cd5baf6298c672
matildavasquezdevi <mvasquezdevillaine@gmail.com>
2025-04-26 23:11:21 -0400

    archivo cvs de prueba

--- App2.java
+++ App2.java
@@ -4,12 +4,10 @@ public class App2 {
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Error: debes pasar exactamente un argumento (el archivo CSV).");
-            System.out.println("Uso correcto: java App2 cultivos.csv");
             return;
         }
 
         String nombreArchivo = args[0];
-        System.out.println("Archivo recibido: " + nombreArchivo);
 
         File archivo = new File(nombreArchivo);
         if (!archivo.exists()) {
@@ -22,8 +20,6 @@ public class App2 {
             return;
         }
 
-        System.out.println("Archivo verificado correctamente. 🎯");
-        
-        // Aquí seguirías leyendo el archivo...
+        System.out.println("Archivo verificado correctamente.");
     }
 }
